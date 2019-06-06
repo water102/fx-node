03bee2f72e6c9580d111e1b14b31a3061a3cf61b
Hoan Nguyen Ngoc <water102.f1@gmail.com>
2019-06-06 10:53:43 +0700

    publish 1.0.3

--- fx.jwt.js
+++ fx.jwt.js
@@ -1,39 +1,51 @@
 const jwt = require('jsonwebtoken');
 
 class FxJwt {
-    sign(accountId, clientId) {
-        let {
-            secretKey,
-            expiresIn
-        } = global.config.jwt;
-        const token = jwt.sign({
-            accountId,
-            clientId
-        }, secretKey, {
-                expiresIn
-            });
-        return token;
-    }
+  signAccessToken(accountId, clientId) {
+    let { secretKey, expiresIn } = global.config.jwt;
+    const token = jwt.sign(
+      {
+        accountId,
+        clientId
+      },
+      secretKey,
+      {
+        accessTokenExpiresIn
+      }
+    );
+    return token;
+  }
 
-    validate(req, res, next) {
-        let {
-            secretKey
-        } = global.config.jwt;
-        jwt.verify(req.headers['x-access-token'],
-            secretKey,
-            (err, decoded) => {
-                if (err) {
-                    res.json({
-                        status: "error",
-                        message: err.message
-                    });
-                } else {
-                    // add user id to request
-                    req.tokenInfo = decoded;
-                    next();
-                }
-            });
-    }
+  signRefeshToken(accountId, clientId) {
+    let { secretKey, expiresIn } = global.config.jwt;
+    const token = jwt.sign(
+      {
+        accountId,
+        clientId
+      },
+      secretKey,
+      {
+        refeshTokenExpiresIn
+      }
+    );
+    return token;
+  }
+
+  validate(req, res, next) {
+    let { secretKey } = global.config.jwt;
+    jwt.verify(req.headers['x-access-token'], secretKey, (err, decoded) => {
+      if (err) {
+        res.json({
+          status: -1,
+          message: err.message
+        });
+      } else {
+        // add user id to request
+        req.tokenInfo = decoded;
+        next();
+      }
+    });
+  }
 }
 
 module.exports = new FxJwt();
